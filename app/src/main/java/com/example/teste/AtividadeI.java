cb2c75dc14a8994ae4fd1d5ba48d76061d43acb9
eloahbaracho <eloahbarachodossantos@gmail.com>
2024-03-06 11:14:45 -0300

    03 commit - Salvar campos

--- app/src/main/java/com/example/teste/AtividadeI.java
+++ app/src/main/java/com/example/teste/AtividadeI.java
@@ -1,17 +1,35 @@
 package com.example.teste;
 
 import android.os.Bundle;
-import android.widget.TextView;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.desenvolvimentoweb.R;
 
 public class AtividadeI extends AppCompatActivity {
 
     @Override
     public void onCreate(Bundle bundle) {
         super.onCreate(bundle);
-        TextView texto = new TextView(this);
-        texto.setText("Olá, mundo!");
-        setContentView(texto);
+
+        EditText editNome = findViewById(R.id.camponome);
+        EditText editTel = findViewById(R.id.campotelefone);
+
+        Button buttonSalvar = findViewById(R.id.salvar);
+        buttonSalvar.setOnClickListener( e -> {
+            Log.i("Agenda_Contato", "Nome " + editNome.getText());
+            Log.i("Agenda_Contato", "Telefone" + editTel.getText());
+        });
+
+
+
+       // TextView texto = new TextView(this);
+       // texto.setText("Olá, mundo!");
+       // setContentView(texto);
+
+        setContentView(R.layout.principal_layout);
     }
 }
